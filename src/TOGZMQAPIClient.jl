43b10e4ebee52b83fe53b7850a2ec30ec9236d02
1m1 <imi@1m1.io>
2026-05-24 22:43:38 -0500

    1.0.0

---
+++ src/TOGZMQAPIClient.jl
@@ -0,0 +1,30 @@
+module TOGZMQAPIClient
+
+using ZMQ, TOGZMQAPIServer, Serialization, TOGZMQ
+
+const SOCKET = Ref{Socket}()
+
+function awaken(socketlocation)
+    @show "TOGZMQAPIClient.awaken(socketlocation)", socketlocation
+    SOCKET[] = Socket(REQ)
+    connect(SOCKET[], socketlocation)
+end
+# const FUNCTIONS = TOGZMQAPIServer.FUNCTIONS
+# function call(f::Function, x...)
+function call(f::Symbol, x...)
+    @show "TOGZMQAPIClient.call", x, typeof(x), length(x)
+    # buffer = IOBuffer()
+    # serialize(buffer, TOGZMQAPIServer.APIData(f, x))
+    # message = Message(take!(buffer))
+    # @show "TOGZMQAPIClient.call", message
+    # send(SOCKET[], message)
+    TOGZMQ.send(SOCKET[], TOGZMQAPIServer.APIData(f, x))
+    @show "TOGZMQAPIClient.call, send"
+    # deserialize(IOBuffer(recv(SOCKET[])))
+    TOGZMQ.recv(SOCKET[])
+    # @show "TOGZMQAPIClient.call, x", x
+    # x
+end
+call(f::Symbol) = call(f, nothing)
+
+end
