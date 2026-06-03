6f4145daec6da482f5dc4700981638494cc6e04e
1m1 <imi@1m1.io>
2026-06-03 17:39:25 -0500

    4.0.0

--- src/TOGZMQAPIClient.jl
+++ src/TOGZMQAPIClient.jl
@@ -6,14 +6,15 @@ using TOGZMQAPIServer, TOGZMQ
 const SOCKET = Ref{Socket}()
 
 function awaken(socketlocation)
-    @show "TOGZMQAPIClient.awaken(socketlocation)", socketlocation
+    @show "TOGZMQAPIClient.awaken", socketlocation
     SOCKET[] = Socket(REQ)
     connect(SOCKET[], socketlocation)
 end
 function call(f::Symbol, x...)
-    @show "TOGZMQAPIClient.call", x, typeof(x), length(x)
+    @show "TOGZMQAPIClient.call", f, x, typeof(x), length(x)
     TOGZMQ.send(SOCKET[], "", false, "", Symbol(""), "", TOGZMQAPIServer.APIData(f, x))
     _, _, _, _, _, information = TOGZMQ.recv(SOCKET[])
+    @show "TOGZMQAPIClient.call", typeof(information), information
     information
 end
 call(f::Symbol) = call(f, nothing)
