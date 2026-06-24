89ddef053c97bd3c5ca4da0b8f9527c29be530c6
1m1 <imi@1m1.io>
2026-06-23 23:06:34 -0500

    9.0.0

--- src/TOGZMQAPIClient.jl
+++ src/TOGZMQAPIClient.jl
@@ -8,10 +8,11 @@ function awaken(socketlocation)
     # @show "TOGZMQAPIClient.awaken", socketlocation
     socket = Socket(REQ)
     connect(socket, socketlocation)
+    socket
 end
 function call(socket::Socket, f::Symbol, x...)
     # @show "TOGZMQAPIClient.call", f, x, typeof(x), length(x)
-    TOGZMQ.send(socket, TOGZMQAPIServer.APIData(f, x))
+    TOGZMQ.send(socket, TOGZMQAPIServer.APIData(f, x...))
     _, _, _, _, _, information = TOGZMQ.receive(socket)
     # @show "TOGZMQAPIClient.call", typeof(information) #, information
     information
